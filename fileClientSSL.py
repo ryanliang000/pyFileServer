f131d4ad55adfca639e1ad6cec3255988cf60d60
ryanliang000 <mail>
2019-06-11 23:01:00 +0800

    translate support SSL

---
+++ fileClientSSL.py
@@ -0,0 +1,132 @@
+#!/usr/bin/python
+import sys,os
+from socket import *
+import ssl
+if (len(sys.argv) != 5):
+    print("Usage: {} put/get ip port filename".format(sys.argv[0]))
+    sys.exit(1)
+
+opentype = sys.argv[1]
+host = sys.argv[2] 
+port = eval(sys.argv[3]) 
+filename = sys.argv[4]
+bufsize = 1480
+addr = (host,port)
+saveDir = "./received/"
+
+def SendFile(tcpClient, filepathname):
+    fd = open(filepathname, 'rb')
+    data = fd.read()
+    fd.close()
+    tcpClient.send(data)
+
+def SendFileToServer(addr, filename):
+    sendfilename = os.path.basename(filename)
+    #check file exist
+    if (not os.path.exists(filename)):
+        print('file<{}> not exist, please check'.format(filename))
+        sys.exit(1)
+    filesize = os.path.getsize(filename) 
+    tcpClient = socket(AF_INET, SOCK_STREAM)
+    try:
+        tcpClient = ssl.wrap_socket(tcpClient)
+        tcpClient.connect(addr)
+        #step1: send command flag, filenane, filesize
+        print('send file name and size to file server...') 
+        cmd = '#SENDFILE#\n{}\n{}'.format(filename, filesize)
+        tcpClient.send(cmd.encode('utf8'))
+        recv = tcpClient.recv(bufsize)
+        if (recv != b'#OK#'):
+            raise(Exception('file server return error: {}'.format(recv)))
+    
+        #step2: send file binary
+        print('send file: ' + filename)
+        SendFile(tcpClient, filename)
+        recv = tcpClient.recv(bufsize).decode('utf8')
+        if (not recv.startswith('#OK#')):
+            raise(Exception('file server return error: {}'.format(recv)))
+        print('server return: ' + recv.split('\n')[1])
+        print('send file success!')
+    except Exception as e:
+        print(e)
+    finally:
+        tcpClient.close();
+
+#return: uniquefilename,ischanged
+def GetUniqueFilename(servDir, filename):
+    if (not os.path.exists(servDir + filename)): 
+        return filename, False
+    nPos = filename.rfind('.')
+    if (nPos != -1):
+        prefix = filename[:nPos]
+        posfix = filename[nPos:]
+    else:
+        prefix = filename
+        posfix = ""
+    nSeq = 1 
+    while(True):
+        newfilename = prefix + str(nSeq) + posfix 
+        if (not os.path.exists(servDir + newfilename)):
+            return newfilename,True
+        nSeq += 1
+
+def ReceiveFile(tcpClient, filepathname, filesize):
+    try:
+        tempfilepathname = filepathname + ".tmp"
+        fd = open(tempfilepathname, 'wb')
+        nrecv = 0
+        while(True):
+            data = tcpClient.recv(bufsize)
+            if (not data):
+                fd.close()
+                raise(Exception("receive file abnormal"))
+            fd.write(data)
+            nrecv += len(data)
+            if (nrecv >= filesize):
+                break;
+        fd.close()
+        os.rename(tempfilepathname, filepathname)
+    finally:
+        if (os.path.exists(tempfilepathname)):
+            os.remove(tempfilepathname)
+
+def GetFileFromServer(addr, filename):
+    tcpClient = socket(AF_INET, SOCK_STREAM)
+    try:
+        tcpClient.connect(addr)
+        tcpClient = ssl.wrap_socket(tcpClient) 
+        #step1: send get size command flag, filenane
+        print('send request file name to file server...') 
+        cmd = '#GETFILE#\n{}'.format(filename)
+        tcpClient.send(cmd.encode('utf8'))
+        recv = tcpClient.recv(bufsize)
+        if (not recv.startswith(b'#OK#')):
+            raise(Exception('file server return error: {}'.format(recv)))
+        infos = recv.decode('utf8').split('\n')
+        if (len(infos) < 2):
+            raise(Exception('get file size failed. error return: {}'.format(recv)))
+        filesize = int(infos[1])
+        print("receive file size: " + str(filesize))
+
+        #step2: get file command flag, filename
+        tcpClient.send(b'#GETFILE#')
+        filename = os.path.basename(filename)
+        filename, ischanged = GetUniqueFilename(saveDir,filename)
+        if (ischanged):
+            print("file existed, change file name to " + filename)
+        filepathname = saveDir + filename
+        ReceiveFile(tcpClient, filepathname, filesize)
+        tcpClient.send(b'#OK#')
+        print("receive and save file: " + filepathname)
+    except Exception as e:
+        print(e)
+    finally:
+        tcpClient.close();
+
+if (opentype == 'put'):
+    SendFileToServer(addr, filename)
+elif (opentype == 'get'):
+    if (not os.path.exists(saveDir)):
+        os.mkdir(saveDir)
+    GetFileFromServer(addr, filename)
+
---
+++ fileServerSSL.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python
+import sys,os
+from socket import *
+from time import ctime
+import ssl
+if (len(sys.argv) < 4):
+    print("Usage: {} port certfile certkeyfile".format(sys.argv[0]))
+    sys.exit(1)
+
+host = ''
+port = eval(sys.argv[1])
+certfile = sys.argv[2]
+keyfile = sys.argv[3]
+bufsize = 1500
+addr = (host,port)
+servDir = './servPath/'
+
+if (not os.path.exists(certfile)):
+    print("certficate file not exist: " + certfile)
+    sys.exit(1)
+if (not os.path.exists(keyfile)):
+    print("certficate key file not exist: " + keyfile)
+    sys.exit(1)
+if (not os.path.exists(servDir)):
+    os.mkdir(servDir)
+
+#return: uniquefilename,ischanged
+def GetUniqueFilename(servDir, filename):
+    if (not os.path.exists(servDir + filename)): 
+        return filename, False
+    nPos = filename.rfind('.')
+    if (nPos != -1):
+        prefix = filename[:nPos]
+        posfix = filename[nPos:]
+    else:
+        prefix = filename
+        posfix = ""
+    nSeq = 1
+    while(True):
+        newfilename = prefix + str(nSeq) + posfix 
+        if (not os.path.exists(servDir + newfilename)):
+            return newfilename,True
+        nSeq += 1
+
+def ReceiveFile(tcpClient, filepathname, filesize):
+    try:
+        tempfilepathname = filepathname + ".tmp"
+        fd = open(tempfilepathname, 'wb')
+        nrecv = 0
+        while(True):
+            data = tcpClient.recv(bufsize)
+            if (not data):
+                fd.close()
+                raise(Exception("receive file abnormal"))
+            fd.write(data)
+            nrecv += len(data)
+            if (nrecv >= filesize):
+                break;
+        fd.close()
+        os.rename(tempfilepathname, filepathname)
+    finally:
+        if (os.path.exists(tempfilepathname)):
+            os.remove(tempfilepathname)
+
+def SendFile(tcpClient, filepathname):
+    fd = open(filepathname, 'rb')
+    data = fd.read()
+    fd.close()
+    tcpClient.send(data)
+
+tcpServer = socket(AF_INET, SOCK_STREAM)
+tcpServer.bind(addr)
+tcpServer.listen(5)  #5-max links limit
+n = 0
+try:
+    while True:
+        print('Waiting for connection...')
+        tcpClient, addr = tcpServer.accept()
+        n+=1
+        print('No.={}, connect from {}'.format(n,addr))
+        
+        tempfilepathname = servDir + 'default.tmp'
+        try:
+            tcpClient = ssl.wrap_socket(tcpClient, server_side=True,  
+                        certfile=certfile, keyfile=keyfile, ssl_version=ssl.PROTOCOL_TLSv1_2)
+            #step 0: verify client
+            data = tcpClient.recv(bufsize)
+            flag = data.decode('utf8')
+            cmdinfos = flag.split('\n')
+            if (cmdinfos[0] not in ('#SENDFILE#', '#GETFILE#')):
+                raise(Exception('cmd verify failed: ' + cmdinfos[0])) 
+            cmd = cmdinfos[0]
+            print("receive cmd: " + cmd)
+            if (cmd == "#SENDFILE#"):
+                filename = cmdinfos[1]
+                filesize = int(cmdinfos[2])
+                print("receive file name: " + filename)
+                print("receive file size: " + str(filesize))
+                tcpClient.send(b'#OK#')
+                #check filename
+                filename, ischanged = GetUniqueFilename(servDir,filename)
+                if (ischanged):
+                    print("file existed, change file name to " + filename)
+
+                #step 2: read buffer write to file
+                filepathname = servDir + filename
+                ReceiveFile(tcpClient, filepathname, filesize)
+                tcpClient.send(b'#OK#\nsave file to {}'.format(filename).encode('utf8'))
+                print("receive and save file: " + filepathname)
+            elif (cmd == '#GETFILE#'):
+                filepathname = servDir + cmdinfos[1] 
+
+                #step 1: check file and return size
+                print("receive: request file " + filepathname)
+                #check file exist
+                if (not os.path.exists(filepathname)):
+                    tcpClient.send('file not exist')
+                    raise(Exception('file<{}> not exist, please check'.format(filepathname)))
+                filesize = os.path.getsize(filepathname)
+                tcpClient.send(b'#OK#\n{}'.format(filesize).encode('utf8'))
+                
+                #step 2: send file
+                data = tcpClient.recv(bufsize)
+                if (data != b'#GETFILE#'):
+                    tcpClient.send('check response error')                    
+                    raise(Exception('client response error: ' + data))
+                print('send file: ' + filepathname)
+                SendFile(tcpClient, filepathname)
+                recv = tcpClient.recv(bufsize)
+                if (recv != b'#OK#'):
+                    raise(Exception('client return error: {}'.format(recv)))
+                print('send file success!')
+
+        except Exception as e:
+            print(e.args)
+        finally:
+            tcpClient.close()
+finally:
+    tcpServer.close()
+    print("Socket closed.")
